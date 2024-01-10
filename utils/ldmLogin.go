50903d9f91d76d462e34b7ae29135b3f87bc41d0
bhreddy4268 <156051732+bhreddy4268@users.noreply.github.com>
2024-01-10 10:09:58 +0530

    Update ldmLogin.go

--- utils/ldmLogin.go
+++ utils/ldmLogin.go
@@ -7,9 +7,9 @@ import (
 	"net/http"
 )
 
-func LDMLogin(URL string, client *http.Client, User string, Pass string) (*http.Response, *http.Client) {
+func LDMLogin(URL string, client *http.Client, User string, Pass string, output string) (*http.Response, *http.Client) {
 
-	values := map[string]string{"user": User, "password": Pass, "namespace": "Native", "infaToken": infaToken}
+	values := map[string]string{"user": User, "password": Pass, "namespace": "Native", "infaToken": output}
 
 	jsonValue, _ := json.Marshal(values)
 	//pass the values to the request's body
